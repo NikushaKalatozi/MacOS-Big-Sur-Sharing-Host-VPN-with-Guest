bcc14edae161bf37e63e70772804f00f2b67ee11
Nikusha Kalatozi <officialnikusha@gmail.com>
2021-04-19 10:54:05 +0300

    Update vpn_tunnel.sh
    Script now is fully automatic, in this update we added some fancy ASCII text, we updated performance and now we don't have to manually modify script to input bridge100 IP address, script greps it automatically from the ipconfig output.

--- vpn_tunnel.sh
+++ vpn_tunnel.sh
@@ -1,6 +1,25 @@
+cat << "EOF" 
+ __     __  _______   __    __        ________                                        __ 
+/  |   /  |/       \ /  \  /  |      /        |                                      /  |
+$$ |   $$ |$$$$$$$  |$$  \ $$ |      $$$$$$$$/__    __  _______   _______    ______  $$ |
+$$ |   $$ |$$ |__$$ |$$$  \$$ |         $$ | /  |  /  |/       \ /       \  /      \ $$ |
+$$  \ /$$/ $$    $$/ $$$$  $$ |         $$ | $$ |  $$ |$$$$$$$  |$$$$$$$  |/$$$$$$  |$$ |
+ $$  /$$/  $$$$$$$/  $$ $$ $$ |         $$ | $$ |  $$ |$$ |  $$ |$$ |  $$ |$$    $$ |$$ |
+  $$ $$/   $$ |      $$ |$$$$ |         $$ | $$ \__$$ |$$ |  $$ |$$ |  $$ |$$$$$$$$/ $$ |
+   $$$/    $$ |      $$ | $$$ |         $$ | $$    $$/ $$ |  $$ |$$ |  $$ |$$       |$$ |
+    $/     $$/       $$/   $$/          $$/   $$$$$$/  $$/   $$/ $$/   $$/  $$$$$$$/ $$/ 
+                                                                                         
+                                                                                         
+                                                                                         
+
+
+EOF
 export utunid=$(ifconfig | grep -B 1 -- -- | awk -F ":" '{print $1}' | grep ^u)
-sudo pfctl -a com.apple.internet-sharing/shared_v4 -s nat #2>/dev/null
-sudo pfctl -a com.apple.internet-sharing/shared_v4 -s nat #2>/dev/null > newrules.conf
-echo "nat on $utunid inet from 192.168.72.0/24 to any -> ($utunid) extfilter ei" > newrules.conf
-sudo pfctl -a com.apple.internet-sharing/shared_v4 -N -f newrules.conf #2>/dev/null
-echo $utunid
+export bridge100ip=$(ifconfig bridge100 | grep 'inet ' | awk '{print $2}' | sed 's:[^.]*$:0/24:') 
+sudo pfctl -a com.apple.internet-sharing/shared_v4 -s nat 2>/dev/null
+sudo pfctl -a com.apple.internet-sharing/shared_v4 -s nat 2>/dev/null > newrules.conf
+echo "Setting up a bridge100 and utun network interfaces for you"
+echo "Your bridge100 IP is $bridge100ip , and your utun interface is $utunid" 
+echo "nat on $utunid inet from $bridge100ip to any -> ($utunid) extfilter ei" > newrules.conf
+sudo pfctl -a com.apple.internet-sharing/shared_v4 -N -f newrules.conf 2>/dev/null
+echo "Congrats, now you are currently using $utunid network interface, feel free to tunnel your VPN"
